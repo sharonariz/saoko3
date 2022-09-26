7a33fdd99caa0b6ae27612d8bf1ae5c69683ea16
Sharon GU <sharon.arizbeth@gmail.com>
2022-09-26 10:58:01 -0500

    feat: Se agregó color al título

---
+++ script.js
@@ -0,0 +1,7 @@
+
+console.log("Saokoyabasta")
+document.getElementById("generation").style.color="grey"
+function colorTitulo(){
+  console.log("Pulso el botón de Generation")
+  document.getElementById("generation").style.color = "blue";
+}
