4659de31e10e3b3de8e203b64bfc0ae4c83981c7
Sharon GU <sharon.arizbeth@gmail.com>
2022-09-27 10:37:00 -0500

    Feat: se agregaron mas funciones

--- scripts/funciones.js
+++ scripts/funciones.js
@@ -87,3 +87,20 @@ function factorial(num){
     return (num* factorial(num-1));
 }
 console.log("Factorial de 5 = " + factorial(5));
+
+//+++++++++++REST PARAMETROS+++++++++++++++++++++++
+
+// parameters => operation;
+// (param1, param2) => {
+
+// return data
+// };
+
+function sum( a,b, ... resto){
+    let suma = a+b;
+    resto.forEach(element => {
+        suma+= element; 
+    });
+    return suma;
+}
+console.log("El resultado de sumar varios números es: " + sum(2,3,5,7,3));
